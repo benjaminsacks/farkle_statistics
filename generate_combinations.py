Fixed probability calculation + Added csv column names

generate_combinations.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import math
 from itertools import combinations_with_replacement
 from calculate_points import calculate_points
@@ -11,10 +12,14 @@ if __name__ == "__main__":
             (math.factorial(d + 5)) / (math.factorial(d) * math.factorial(5))
         )
 
-        rolls_points_perms = np.zeros(shape=(num_rolls, d + 2))
+        rolls_points_perms = np.zeros(shape=(num_rolls, d + 3))
         for i, r in enumerate(rolls):
             rolls_points_perms[i][:d] = r
-            rolls_points_perms[i][d] = calculate_points(r)[0]
-            rolls_points_perms[i][d + 1] = count_permutations(r, d) / num_rolls
+            rolls_points_perms[i][[d, d + 1]] = calculate_points(r)
+            rolls_points_perms[i][d + 2] = count_permutations(r, d) / 6**d
 
-        np.savetxt(f"rolls\\{d}_dice_rolls.csv", rolls_points_perms, delimiter=",")
+        column_names = [f"d{n}" for n in range(1, d + 1)] + ["points", "dice_remaining", "probability"]
+        df = pd.DataFrame(rolls_points_perms, columns=column_names)
+        df.iloc[:, :-1] = df.iloc[:, :-1].astype(int)
+
+        df.to_csv(f"rolls\\{d}_dice_rolls.csv", index=False)