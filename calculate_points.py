3f93137e26ce0caa416aef2297a554a79323feb5
Benjamin Sacks <nadoben21@gmail.com>
2024-04-14 13:52:21 -0700

    Changed function name

--- calculate_points.py
+++ calculate_points.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-def calculate_points(roll):
+
+def calculate_max_points(roll):
     roll = np.array(roll).astype(int)
     counts = np.zeros(7)
     raw_counts = np.bincount(roll)
-    counts[:len(raw_counts)] += raw_counts
-    
+    counts[: len(raw_counts)] += raw_counts
+
     points = 0
     dice_remaining = 6
 
@@ -13,15 +14,15 @@ def calculate_points(roll):
     for i in range(1, 7):
         if counts[i] == 6:
             return 3000, 6
-        
+
     # Check for 1-6 straight
     if np.all(counts[1:] == 1):
         return 1500, 6
-    
+
     # Check for 3 pairs
     if np.sum(counts == 2) == 3:
         return 1500, 6
-    
+
     # Check for 2 triplets
     if np.sum(counts == 3) == 2:
         return 2500, 6
@@ -33,10 +34,10 @@ def calculate_points(roll):
             dice_remaining -= 5
 
             # Check remaining points
-            if counts[1] == 1: 
+            if counts[1] == 1:
                 points += 100
                 dice_remaining = 6
-            if counts[5] == 1: 
+            if counts[5] == 1:
                 points += 50
                 dice_remaining = 6
 
@@ -54,51 +55,61 @@ def calculate_points(roll):
                     points += 500
                     dice_remaining = 6
                     return points, dice_remaining
-            
+
             # Check remaining points
-            if counts[1] == 1: 
+            if counts[1] == 1:
                 points += 100
                 dice_remaining -= 1
-            if counts[5] == 1: 
+            if counts[5] == 1:
                 points += 50
                 dice_remaining -= 1
 
-            if dice_remaining == 0: return points, 6
-            else: return points, dice_remaining
-    
+            if dice_remaining == 0:
+                return points, 6
+            else:
+                return points, dice_remaining
+
     # Check for three of a kind
     for i in range(1, 7):
         if counts[i] == 3:
-            if i == 1: points += 300
-            else: points += i * 100
+            if i == 1:
+                points += 300
+            else:
+                points += i * 100
             dice_remaining -= 3
 
             # Check remaining points
-            if counts[1] in (1, 2): 
+            if counts[1] in (1, 2):
                 points += 100 * counts[1]
                 dice_remaining -= counts[1]
-            if counts[5] in (1, 2): 
+            if counts[5] in (1, 2):
                 points += 50 * counts[5]
                 dice_remaining -= counts[5]
 
-            if dice_remaining == 0: return points, 6
-            else: return points, dice_remaining
-    
+            if dice_remaining == 0:
+                return points, 6
+            else:
+                return points, dice_remaining
+
     # Check remaining points
-    if counts[1] in (1, 2): 
+    if counts[1] in (1, 2):
         points += 100 * counts[1]
         dice_remaining -= counts[1]
-    if counts[5] in (1, 2): 
+    if counts[5] in (1, 2):
         points += 50 * counts[5]
         dice_remaining -= counts[5]
 
-    if points == 0: return 0, 0
-    elif dice_remaining == 0: return points, 6
-    else: return points, dice_remaining
+    if points == 0:
+        return 0, 0
+    elif dice_remaining == 0:
+        return points, 6
+    else:
+        return points, dice_remaining
+
 
 if __name__ == "__main__":
     roll = [1, 1, 1, 1, 1, 1]
-    points, dice_remaining = calculate_points(roll)
+    points, dice_remaining = calculate_max_points(roll)
     print(roll)
     print("Maximum points:", points)
     print("Dice remaining:", dice_remaining)
--- generate_combinations.py
+++ generate_combinations.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import math
 from itertools import combinations_with_replacement
-from calculate_points import calculate_points
+from calculate_points import calculate_max_points
 from count_permutations import count_permutations
 
 if __name__ == "__main__":
@@ -15,10 +15,14 @@ if __name__ == "__main__":
         rolls_points_perms = np.zeros(shape=(num_rolls, d + 3))
         for i, r in enumerate(rolls):
             rolls_points_perms[i][:d] = r
-            rolls_points_perms[i][[d, d + 1]] = calculate_points(r)
+            rolls_points_perms[i][[d, d + 1]] = calculate_max_points(r)
             rolls_points_perms[i][d + 2] = count_permutations(r, d) / 6**d
 
-        column_names = [f"d{n}" for n in range(1, d + 1)] + ["points", "dice_remaining", "probability"]
+        column_names = [f"d{n}" for n in range(1, d + 1)] + [
+            "points",
+            "dice_remaining",
+            "probability",
+        ]
         df = pd.DataFrame(rolls_points_perms, columns=column_names)
         df.iloc[:, :-1] = df.iloc[:, :-1].astype(int)
 
